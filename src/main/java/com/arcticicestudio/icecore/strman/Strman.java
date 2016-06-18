a1d1c0d6770a97e6c1322b1ca0ce0cb779f710b6
Arctic Ice Studio <development@arcticicestudio.com>
2016-06-18 11:47:37 +0200

    Implement public API static method "+ lastIndexOf(String,String)"

--- src/main/java/com/arcticicestudio/icecore/strman/Strman.java
+++ src/main/java/com/arcticicestudio/icecore/strman/Strman.java
@@ -538,6 +538,23 @@ public abstract class Strman {
    *
    * <p>
    *   Returns a negative integer if the value is not found.
+   *   The search runs backwards and is case sensitive.
+   * </p>
+   *
+   * @param value the value to search
+   * @param needle the needle to find
+   * @return the position of the last occurrence of the needle, negative integer if not found
+   */
+  public static int lastIndexOf(final String value, final String needle) {
+    validate(value, NULL_STRING_PREDICATE, NULL_STRING_MSG_SUPPLIER);
+    return lastIndexOf(value, needle, value.length(), true);
+  }
+
+  /**
+   * Returns the index of the last occurrence of the specified needle.
+   *
+   * <p>
+   *   Returns a negative integer if the value is not found.
    *   The search runs backwards.
    * </p>
    *
