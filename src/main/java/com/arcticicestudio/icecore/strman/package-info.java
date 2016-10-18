2bd5d266950c0314407ec87f7b844beacf6a2d52
Arctic Ice Studio <development@arcticicestudio.com>
2016-10-18 21:24:24 +0200

    Update repository URLs in Java source code

--- src/main/java/com/arcticicestudio/icecore/strman/package-info.java
+++ src/main/java/com/arcticicestudio/icecore/strman/package-info.java
@@ -1,25 +1,24 @@
 /*
-+++++++++++++++++++++++++++++++++++++++++++
-title     Package Info                    +
-project   icecore-strman                  +
-file      package-info.java               +
-version   0.3.0                           +
-author    Arctic Ice Studio               +
-email     development@arcticicestudio.com +
-website   http://arcticicestudio.com      +
-copyright Copyright (C) 2016              +
-+++++++++++++++++++++++++++++++++++++++++++
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+title      Package Info                                      +
+project    icecore-strman                                    +
+version    0.3.0                                             +
+repository https://github.com/arcticicestudio/icecore-strman +
+author     Arctic Ice Studio                                 +
+email      development@arcticicestudio.com                   +
+copyright  Copyright (C) 2016                                +
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 */
 /**
  * Represents the public API of the
- * <a href="https://bitbucket.com/arcticicestudio/icecore-strman">"IceCore - Strman"</a> library.
+ * <a href="https://github.com/arcticicestudio/icecore-strman">"IceCore Strman"</a> library.
  * <p>
  *   The main entry point of the package is the {@link com.arcticicestudio.icecore.strman.Strman} class
  *   to manipulate- and modify strings.
  * </p>
  *
  * @author Arctic Ice Studio &lt;development@arcticicestudio.com&gt;
- * @see <a href="https://bitbucket.org/arcticicestudio/icecore-strman">IceCore - Strman</a>
+ * @see <a href="https://github.com/arcticicestudio/icecore-strman">IceCore Strman</a>
  * @version 0.3.0
  */
 package com.arcticicestudio.icecore.strman;
